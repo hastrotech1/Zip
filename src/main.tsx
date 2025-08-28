c00727a7117d78215a29e445b93f0fbbcb203b58
Haastrup Daniel <140532908+hastrotech1@users.noreply.github.com>
2025-08-28 11:05:07 +0000

    send certs-2.2

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const CLIENT_ID = "180625905651-os34vuh7ll8pfuvq5ropkgfm645mt7bn.apps.googleusercontent.com";
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={CLIENT_ID}>
