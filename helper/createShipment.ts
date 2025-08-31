a79a9539fd8fe2a49b94f3857c2e9b6037680322
Haastrup Daniel <140532908+hastrotech1@users.noreply.github.com>
2025-08-31 02:34:27 +0000

    expanded-0556

--- helper/createShipment.ts
+++ helper/createShipment.ts
@@ -1,15 +1,43 @@
 import axios from "axios";
 
+// export async function createShipment(params: {
+//   pickup_location: string;
+//   delivery_location: string;
+//   receiver_phone: string;
+//   estimate_fee: number;
+//   selected_vehicle: string; // vehicle ID
+// }) {
+//   const token = localStorage.getItem("access_token");
+//   if (!token) throw new Error("No access token found");
+
+//   const response = await axios.post(
+//     "https://ziplugs.geniusexcel.tech/api/shipments",
+//     params
+//   );
+  
+//   return response.data;
+// }
+
 export async function createShipment(params: {
   pickup_location: string;
   delivery_location: string;
   receiver_phone: string;
   estimate_fee: number;
-  selected_vehicle: string; // vehicle ID
+  selected_vehicle: string;
 }) {
+  const token = localStorage.getItem("access_token");
+  if (!token) throw new Error("No access token found");
+
   const response = await axios.post(
     "https://ziplugs.geniusexcel.tech/api/shipments",
-    params
+    params,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    }
   );
+
   return response.data;
 }
